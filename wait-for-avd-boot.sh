1632d0d3f9c5c59bfc92fb45a5b3753b9861f96d
Keval Patel <kevalpatel2106@gmail.com>
2017-04-17 14:42:47 +0530

    Update wait-for-avd-boot.sh

--- wait-for-avd-boot.sh
+++ wait-for-avd-boot.sh
@@ -1,12 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-OUT=`/sdk/platform-tools/adb shell getprop init.svc.bootanim`
-RES="stopped"
+# Originally written by Ralf Kistner <ralf@embarkmobile.com>, but placed in the public domain
 
-while [[ ${OUT:0:7}  != 'stopped' ]]; do
-		OUT=`/sdk/platform-tools/adb shell getprop init.svc.bootanim`
-		echo 'Waiting for emulator to fully boot...'
-		sleep 5
+set +e
+
+bootanim=""
+failcounter=0
+timeout_in_sec=360
+
+until [[ "$bootanim" =~ "stopped" ]]; do
+  bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+  if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
+    || "$bootanim" =~ "running" ]]; then
+    let "failcounter += 1"
+    echo "Waiting for emulator to start"
+    if [[ $failcounter -gt timeout_in_sec ]]; then
+      echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
+      exit 1
+    fi
+  fi
+  sleep 1
 done
 
-echo "Emulator booted!"
+echo "Emulator is ready"
